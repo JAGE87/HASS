71c81f4d9a328107c974cb83f1124cc0f53de9e4
JAGE87 <66187488+JAGE87@users.noreply.github.com>
2020-06-19 12:41:17 -0500

    Update procedures.sql

--- db/procedures.sql
+++ db/procedures.sql
@@ -1,26 +1,34 @@
-USE company;
+USE CPQ;
 
 DELIMITER $$
-USE `company`$$
+USE `CPQ`$$
 
-CREATE PROCEDURE `employeeAddOrEdit` (
-  IN _id INT,
-  IN _name VARCHAR(45),
-  IN _salary INT
+CREATE PROCEDURE `modificar` (
+    IN _id VARCHAR(10),
+    IN _DateCreated DATE,
+    IN _DateModified DATE,
+    IN _DateExpiration DATE,
+    IN _ProductNumber VARCHAR(150),
+    IN _ProductPartNumber VARCHAR(150),
+    IN _ProductName VARCHAR(150),
+    IN _ProductPrice VARCHAR(150),
+    IN _ProductQuantity VARCHAR(150),
+    IN _ProductInventory VARCHAR(150),
+    IN _ProductTypeName VARCHAR(150),
+    IN _ProductCategoryName VARCHAR(150),
+    IN _ProductPromoDiscountAmount VARCHAR(150),
+    IN _ProductPromoDiscountPercent VARCHAR(150),
+    IN _OwnerId VARCHAR(10),
+    IN _OwnerName VARCHAR(30),
+    IN _OwnerUser VARCHAR(30),
+    IN _CustomerName VARCHAR(30),
+    IN _CustomerEmail VARCHAR(30),
+    IN _CustomerPhone VARCHAR(30),
+    IN _CustomerCompany VARCHAR(30)
 )
-BEGIN 
-  IF _id = 0 THEN
-    INSERT INTO employee (name, salary)
-    VALUES (_name, _salary);
 
-    SET _id = LAST_INSERT_ID();
-  ELSE
-    UPDATE employee
-    SET
-    name = _name,
-    salary = _salary
-    WHERE id = _id;
-  END IF;
-
-  SELECT _id AS 'id';
+BEGIN
+		INSERT INTO cotizaciones(id, DateCreated, DateModified, DateExpiration, ProductNumber, ProductPartNumber, ProductName, ProductPrice, ProductQuantity, ProductInventory, ProductTypeName, ProductCategoryName, ProductPromoDiscountAmount, ProductPromoDiscountPercent, OwnerId, OwnerName, OwnerUser, CustomerName, CustomerEmail, CustomerPhone, CustomerCompany)
+        VALUES(_id, _DateCreated, _DateModified, _DateExpiration, _ProductNumber, _ProductPartNumber, _ProductName, _ProductPrice, _ProductQuantity, _ProductInventory, _ProductTypeName, _ProductCategoryName, _ProductPromoDiscountAmount, _ProductPromoDiscountPercent, _OwnerId, _OwnerName, _OwnerUser, _CustomerName, _CustomerEmail, _CustomerPhone, _CustomerCompany);
+    SELECT _id AS id;
 END
